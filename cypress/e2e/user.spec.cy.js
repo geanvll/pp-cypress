9d69f3676c6fb7ef1a55110ba54fbb2ac1dcf7b9
Gean Lopes <geanvll@outlook.com>
2024-01-20 11:14:22 -0300

    User update scenario added

---
+++ cypress/e2e/user.spec.cy.js
@@ -0,0 +1,51 @@
+import userData from '../fixtures/userData.json'
+
+describe('Orange HRM Tests', () => {
+
+  const selectorsList = {
+    usernameField : "[name='username']",
+    passwordFIeld : "[name='password']",
+    loginButton : "[type='submit']",
+    sectionTitleTopBar : ".oxd-topbar-header-breadcrumb-module",
+    dashboardLayot: ".oxd-layout",
+    wrongCredentialAlert : "[role='alert']",
+    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+    firstNameField: "[name='firstName']",
+    lastNameField: "[name='lastName']",
+    genericField: ".oxd-input--active",
+    dateField: "[placeholder='yyyy-mm-dd']",
+    dateCloseButton: ".--close",
+    submitButton: "[type='submit']"
+
+  }
+
+  it.only('User Info Update - Success', () => {
+    cy.visit('/auth/login')
+    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorsList.passwordFIeld).type(userData.userSuccess.password)
+    cy.get(selectorsList.loginButton).click()
+    cy.location('pathname').should('equal','/web/index.php/dashboard/index')
+    cy.get(selectorsList.dashboardLayot)
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
+    cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
+    cy.get(selectorsList.genericField).eq(3).clear().type('NicknameTest')
+    cy.get(selectorsList.genericField).eq(4).clear().type('Employee')
+    cy.get(selectorsList.genericField).eq(5).clear().type('OtherIdTest')
+    cy.get(selectorsList.genericField).eq(6).clear().type('DriversLicenseTest')
+    cy.get(selectorsList.genericField).eq(7).clear().type('2025-03-10')
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.genericField).eq(8).clear().type('ssnNumberTest')
+    cy.get(selectorsList.genericField).eq(9).clear().type('siNumberTestTest')
+    cy.get(selectorsList.submitButton).eq(0).click()
+    cy.get('body').should('contain', 'Successfully Updated')
+    cy.get('.oxd-toast-close')
+  })
+  it('Login-Fail', () => {
+    cy.visit('/auth/login')
+    cy.get(selectorsList.usernameField).type(userData.userFail.username)
+    cy.get(selectorsList.passwordFIeld).type(userData.userFail.password)
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsList.wrongCredentialAlert)
+  })
+})
